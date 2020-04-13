5821576ff7065f54a4c7f2b1958e7c8b3fe00e1c
Hildeberto Mendonca <me@hildeberto.com>
2020-04-13 16:57:54 -0400

    Trying to use pure javascript to process the RSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   let reads = $('#reads');
 
-  $.get("/books/feed.xml", function (data) {
-    console.log(data);
-  });
+  const RSS_URL = `https://www.hildeberto.com/books/feed.xml`;
+
+  fetch(RSS_URL)
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => console.log(data));
 
   reads.html("Feed from the book section");
 });

8c34e31fa56639b4a9b3b40add82acb195033a30
Hildeberto Mendonca <me@hildeberto.com>
2020-04-13 16:40:51 -0400

    Removed invalid javascript code

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,3 @@ $(document).ready(function() {
 
   reads.html("Feed from the book section");
 });
-
-
-{% for book in site.data.books %}
-  <h3>{{ book.title }} <small>by {{ book.author }}</small></h3>
-  <img align="left" border="0" style="margin-right: 10px;" width="90" src="{{ book.image }}" >
-  <p>{{ book.description }}</p>
-  <div class="progress">
-    <div class="progress-bar" role="progressbar" style="width: {{ book.progress }}%;" aria-valuenow="{{ book.progress }}" aria-valuemin="0" aria-valuemax="100">{{ book.progress }}%</div>
-  </div>
-{% endfor %}

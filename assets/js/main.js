a2afb352eef3283e98e4288c6038e06960658dd3
Hildeberto Mendonca <me@hildeberto.com>
2021-08-03 22:55:02 -0400

    Adjustments in the layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,6 @@ $(document).ready(function() {
             <h3><a href="/books${el.querySelector("link").innerHTML}" target="_blank">${el.querySelector("title").innerHTML}</a> <small>${el.querySelector("author").innerHTML}</small></h3>
             <img align="left" border="0" style="margin-right: 10px;" width="90" src="${el.querySelector("image").innerHTML}">
             <p>${el.querySelector("summary").innerHTML}</p>
-            <div class="progress">
-              <div class="progress-bar" role="progressbar" style="width: ${el.querySelector("progress").innerHTML}%;" aria-valuenow="${el.querySelector("progress").innerHTML}" aria-valuemin="0" aria-valuemax="100">${el.querySelector("progress").innerHTML}%</div>
-            </div>
           `;
         });
         reads.insertAdjacentHTML("beforeend", html);

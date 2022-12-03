a86a93206a40c66b42ff5eae3df0d77c2d0739dc
Hildeberto Mendonca <me@hildeberto.com>
2022-12-03 13:50:37 -0500

    Removed duplications

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,33 +4,17 @@ $(document).ready(function() {
 });
 
 function loadReads() {
-  let reads = document.getElementById("reads");
-
-  const RSS_URL = `https://www.hildeberto.com/books/atom.xml`;
-
-  fetch(RSS_URL)
-    .then(response => response.text())
-    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then(data => {
-        const items = data.querySelectorAll("entry");
-        let html = ``;
-        items.forEach(el => {
-          html += `
-            <h3><a href="/books${el.querySelector("link").innerHTML}" target="_blank">${el.querySelector("title").innerHTML}</a> <small>${el.querySelector("author").innerHTML}</small></h3>
-            <img align="left" border="0" style="margin-right: 10px;" width="90" src="${el.querySelector("image").innerHTML}">
-            <p>${el.querySelector("summary").innerHTML}</p>
-          `;
-        });
-        reads.insertAdjacentHTML("beforeend", html);
-      });
+  loadRSS("reads", "https://www.hildeberto.com/books/atom.xml", "books");
 }
 
 function loadSports() {
-  let sports = document.getElementById("sports");
+  loadRSS("sports", "https://www.hildeberto.com/sports/atom.xml", "sports");
+}
 
-  const RSS_URL = `https://www.hildeberto.com/sports/atom.xml`;
+function loadRSS(contentElement, url, target) {
+  let element = document.getElementById(contentElement);
 
-  fetch(RSS_URL)
+  fetch(url)
     .then(response => response.text())
     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
     .then(data => {
@@ -38,11 +22,12 @@ function loadSports() {
         let html = ``;
         items.forEach(el => {
           html += `
-            <h3><a href="/sports${el.querySelector("link").innerHTML}" target="_blank">${el.querySelector("title").innerHTML}</a> <small>${el.querySelector("author").innerHTML}</small></h3>
+            <h3><a href="/${target}${el.querySelector("link").innerHTML}" target="_blank">${el.querySelector("title").innerHTML}</a> 
+            <small>${el.querySelector("author").innerHTML}</small></h3>
             <img align="left" border="0" style="margin-right: 10px;" width="90" src="${el.querySelector("image").innerHTML}">
             <p>${el.querySelector("summary").innerHTML}</p>
           `;
         });
-        sports.insertAdjacentHTML("beforeend", html);
+        element.insertAdjacentHTML("beforeend", html);
       });
 }
